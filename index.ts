78d60666f7279bafa8af583c36c499372d090233
jinyongp <dev.jinyongp@gmail.com>
2023-10-16 12:09:45 +0900

    chore: eslint 추가

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const PORT = 8080;
+
+Bun.serve({
+  port: PORT,
+  async fetch(request) {
+    return new Response("Hello, World!", { status: 200 });
+  },
+});
+
+console.log(`Listening on http://localhost:${PORT}`);
