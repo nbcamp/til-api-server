10e7571fdbb6a7b99ac682490a3b547ff8397ec3
jinyongp <dev.jinyongp@gmail.com>
2023-10-16 22:59:04 +0900

    feat: 데이터 모델 정리

---
+++ prisma/migrations/20231016084528_create_models/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT,
+    "profile_url" TEXT,
+    "provider" TEXT,
+    "provider_id" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "last_signed_at" DATETIME,
+    "deleted_at" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "blogs" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "owner_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "rss" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "blogs_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "blog_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "rss" TEXT NOT NULL,
+    "publishedAt" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "posts_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "tags" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "keyword_tag_pairs" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "blog_id" INTEGER NOT NULL,
+    "keyword" TEXT NOT NULL,
+    "tag_id" INTEGER NOT NULL,
+    CONSTRAINT "keyword_tag_pairs_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "keyword_tag_pairs_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "tags" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "providerIndex" ON "users"("provider", "provider_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "keywordTagIndex" ON "keyword_tag_pairs"("keyword", "tag_id");
