be1d604dcd697007c141ff11f3937b909a776dd2
jinyongp <dev.jinyongp@gmail.com>
2023-10-19 23:17:51 +0900

    feat: prisma generate 생성 위치 변경

--- prisma/migrations/20231019134253_create_data_models/migration.sql
+++ prisma/migrations/20231019134253_create_data_models/migration.sql
@@ -21,37 +21,46 @@ CREATE TABLE "blogs" (
     "primary" BOOLEAN NOT NULL DEFAULT false,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL,
-    CONSTRAINT "blogs_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "blogs_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "posts" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "blog_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "rss" TEXT NOT NULL,
     "publishedAt" DATETIME NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL,
-    CONSTRAINT "posts_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "posts_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "post_tags" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "post_id" INTEGER NOT NULL,
+    "tag_id" INTEGER NOT NULL,
+    CONSTRAINT "post_tags_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "post_tags_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "tags" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "keyword_tag_maps" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "blog_id" INTEGER NOT NULL,
+    "keyword" TEXT NOT NULL,
+    "tags" TEXT NOT NULL,
+    CONSTRAINT "keyword_tag_maps_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "tags" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "keyword_tag_pairs" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "blog_id" INTEGER NOT NULL,
-    "keyword" TEXT NOT NULL,
-    "tag_id" INTEGER NOT NULL,
-    CONSTRAINT "keyword_tag_pairs_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "keyword_tag_pairs_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "tags" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -61,4 +70,7 @@ CREATE UNIQUE INDEX "providerIndex" ON "users"("provider", "provider_id");
 CREATE UNIQUE INDEX "userNameIndex" ON "blogs"("user_id", "name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "keywordTagIndex" ON "keyword_tag_pairs"("keyword", "tag_id");
+CREATE UNIQUE INDEX "postTagIndex" ON "post_tags"("post_id", "tag_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "blogKeywordIndex" ON "keyword_tag_maps"("blog_id", "keyword");
